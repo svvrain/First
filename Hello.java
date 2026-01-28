45678ee4788dedc7082b237d508acbc7b193153d
Viktor Smirnov <svv.mutabor@gmail.com>
2026-01-28 17:48:50 +0500

    Second

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, 1!");
         System.out.println("Hello, 2!");
+        System.out.println("Hello, 3!");
+
 
     }
 }
