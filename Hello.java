f5cc9f03403eb70660b77a47e40861b612203bd8
Viktor Smirnov <svv.mutabor@gmail.com>
2026-01-28 17:29:08 +0500

    change Message1

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, 1!");
+        System.out.println("Hello, 2!");
+
     }
 }
